ce21fca1edd10afc07b62950658f8e23892e414d
Christopher Glenn <Chrisglenn519@yahoo.com>
2020-05-15 04:32:55 -0500

    unsure

--- javascript/index.js
+++ javascript/index.js
@@ -1,14 +1,32 @@
 $(document).ready(function(){
-    $('.event-container').show()
+    // $('.event-container').show()
     $('.event-container').hide()
+    $('.menu-container').hide()
+    $('.banner2').hide()
+    setInterval(function(){
+        let num = 1;
+        switch(num){
+            case 1:
+                $('.banner1').hide()
+                $('.banner2').fadeIn(500) 
+        }            
+        
+    },3000)
 })
 $('#nav-event').on('click',function(){
     $('.home-container').hide()
+    $('.menu-container').hide()
     $('.event-container').show()
 })
 
 $('#nav-home').on('click', function(){
     $('.event-container').hide()
+    $('.menu-container').hide()
     $('.home-container').show()
 })
 
+$('#nav-menu').on('click',function(){
+    $('.event-container').hide()
+    $('.home-container').hide()
+    $('.menu-container').show()
+})
