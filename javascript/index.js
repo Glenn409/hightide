5787794b5b8589b9786570f419da4ac319bf7650
Chris Glenn <chrisglenn519@yahoo.com>
2020-05-15 17:16:32 -0500

    top nav bar done

--- javascript/index.js
+++ javascript/index.js
@@ -3,15 +3,28 @@ $(document).ready(function(){
     $('.event-container').hide()
     $('.menu-container').hide()
     $('.banner2').hide()
+    $('.banner3').hide()
+    let num = 1;
     setInterval(function(){
-        let num = 1;
         switch(num){
             case 1:
                 $('.banner1').hide()
                 $('.banner2').fadeIn(500) 
+                num++
+                break;
+            case 2:
+                $('.banner2').hide()
+                $('.banner3').fadeIn(500)
+                num++
+                break;
+            case 3:
+                $('.banner3').hide()
+                $('.banner1').fadeIn(500)
+                num = 1
+                break;
         }            
         
-    },3000)
+    },4000)
 })
 $('#nav-event').on('click',function(){
     $('.home-container').hide()
