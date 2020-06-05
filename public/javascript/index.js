7d65ea0a3fd07766aed9e165a74a4dc6139576c6
Christopher Glenn <Chrisglenn519@yahoo.com>
2020-06-05 09:44:03 -0500

    test

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -176,6 +176,7 @@ $('#mobile-menu').on('click',function(){
     $('.home-container').hide()
     $('.contact-container').hide()
     $('.menu-container').show()
+    $('#drinkheader').css('border-bottom','4px solid rgb(44, 44, 44) ')
     $('html, body').animate({
         scrollTop: $("#circle-logo").offset().top
       }, 200)
